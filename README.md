1b156d7d5ae6008293efe2054cf441379a7380ae
Kazutaka Sakoda <kazutaka.sakoda@gmail.com>
2020-01-17 14:52:43 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pwsh-notebook
+ 
---
+++ Tools/SetupPwsh6RemotingForWindowsHosts.ps1
@@ -0,0 +1,20 @@
+# Configure PSRemoting over ssh on Powershell Core
+$pwshVer = '6.2.3'
+$pwshPath = "c:\pwsh$pwshVer"
+
+Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v$pwshVer/PowerShell-$pwshVer-win-x64.zip" -OutFile "pwsh.zip"
+Expand-Archive -Path pwsh.zip -DestinationPath $pwshPath
+Remove-Item "pwsh.zip"
+
+Add-WindowsCapability -Online -Name (Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH.Server*').Name
+Start-Service sshd
+Stop-Service sshd
+Push-Location $env:ProgramData\ssh
+$fileContent = Get-Content .\sshd_config
+$fileContent = $filecontent -replace '^#PubkeyAuthentication yes',"PubkeyAuthentication yes"
+$fileContent = $filecontent -replace '^#PasswordAuthentication yes',"PasswordAuthentication yes"
+$fileContent = $filecontent -replace "^# override default of no subsystems", "$&`nSubsystem powershell $pwshPath\pwsh.exe -sshs -NoLogo -NoProfile"
+$filecontent | Set-Content .\sshd_config
+Pop-Location
+Set-Service -Name sshd -StartupType 'Automatic'
+Start-Service sshd
