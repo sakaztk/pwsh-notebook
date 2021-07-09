b0e897bf7bcc39967c971ae324cea676335abd3d
Kazutaka Sakoda <kazutaka.sakoda@gmail.com>
2021-07-09 19:07:05 +0900

    (WinPython) Downloading all softwares first.

--- WinPython/WinPython.ps1
+++ WinPython/WinPython.ps1
@@ -1,4 +1,5 @@
 #Requires -Version 5
+#Requires -RunAsAdministrator
 [CmdletBinding()]
 Param(
     [ValidateSet(,'3.8','3.9')]
@@ -19,10 +20,26 @@ Param(
     [String]$WorkingFolder = $PSScriptRoot
 )
 $ErrorActionPreference = 'Stop'
+Write-Verbose ('$WinPythonVersion = ' + $WinPythonVersion)
+Write-Verbose ('$WinPythonType = ' + $WinPythonType)
+Write-Verbose ('$InstallPowerShell7 = ' + $InstallPowerShell7)
+Write-Verbose ('$InstallDotnetInteractive = ' + $InstallDotnetInteractive)
+Write-Verbose ('$InstallNBExtensions = ' + $InstallNBExtensions)
+Write-Verbose ('$InstallNIIExtensions = ' + $InstallNIIExtensions)
+Write-Verbose ('$InstallPortableGit = ' + $InstallPortableGit)
+Write-Verbose ('$WinPythonPath = ' + $WinPythonPath)
+Write-Verbose ('$NodePath = ' + $NodePath)
+Write-Verbose ('$PowerShell7Path = ' + $PowerShell7Path)
+Write-Verbose ('$PortableGitPath = ' + $PortableGitPath)
+Write-Verbose ('$WorkingFolder = ' + $WorkingFolder)
+Write-Verbose ('$CleanupDownloadFiles = ' + $CleanupDownloadFiles)
 Push-Location $WorkingFolder
 $osBits = ( [System.IntPtr]::Size*8 ).ToString()
-Write-Output '##### WinPython Installation #####'
-if ( $WinPythonType = 'unmarked' ) {
+
+$progressPreference = 'SilentlyContinue'
+
+Write-Verbose 'Downloading latest WinPython...'
+if ( $WinPythonType -eq 'unmarked' ) {
     $pattern = $osBits + '-' + $WinPythonVersion + '.*\d\.exe'
 }
 else {
@@ -30,35 +47,72 @@ else {
 }
 $latestRelease = (Invoke-WebRequest 'https://github.com/winpython/winpython/releases/latest' -UseBasicParsing -Headers @{'Accept'='application/json'}| ConvertFrom-Json).update_url
 $links = (Invoke-WebRequest -uri "https://github.com$($latestRelease)" -UseBasicParsing).Links.href
-$fileUri = ($links | Select-String -Pattern $pattern | Get-Unique).Tostring().Trim()
-$progressPreference = 'SilentlyContinue'
-Write-Output 'Downloading Winpython...'
+Write-Verbose ('$pattern = ' + $pattern)
+Try {
+    $fileUri = ($links | Select-String -Pattern $pattern | Get-Unique).Tostring().Trim()
+}
+Catch {
+    throw "Does not exist WinPython$osBits version $WinPythonVersion $WinPythonType in latest release."
+}
 Invoke-WebRequest -uri "https://github.com$($fileUri)" -OutFile (Join-Path $WorkingFolder 'Winpython.exe') -Verbose
+$wpVer = $fileUri -replace ".*-((\d+\.)?(\d+\.)?(\d+\.)?(\*|\d+)).*\.exe",'$1'
+
+Write-Verbose 'Downloading latest Node.js...'
+$links = (Invoke-WebRequest -uri 'https://nodejs.org/ja/download/' -UseBasicParsing).Links.href
+$pattern = "win-x$osBits.*\.zip"
+$fileUri = ($links | Select-String -Pattern $pattern | Get-Unique).Tostring().Trim()
+Invoke-WebRequest -Uri $fileUri -OutFile (Join-Path $WorkingFolder '\node.zip')
+
+if ( $InstallPortableGit ) {
+    Write-Verbose 'Downloading latest PortableGit...'
+    $latestRelease = (Invoke-WebRequest 'https://github.com/git-for-windows/git/releases/latest' -UseBasicParsing -Headers @{'Accept'='application/json'}| ConvertFrom-Json).update_url
+    $links = (Invoke-WebRequest -uri "https://github.com$($latestRelease)" -UseBasicParsing).Links.href
+    $fileUri = ($links | Select-String -Pattern ".*PortableGit.*$osBits.*\.exe" | Get-Unique).Tostring().Trim()
+    Invoke-WebRequest -uri "https://github.com$($fileUri)" -OutFile (Join-Path $WorkingFolder 'PortableGit.exe') -Verbose
+}
+
+if ( $InstallPowerShell7 ) {
+    Write-Verbose 'Downloading latest PowerShell 7...'
+    $latestRelease = (Invoke-WebRequest -Uri 'https://github.com/PowerShell/PowerShell/releases/latest' -UseBasicParsing -Headers @{'Accept'='application/json'}| ConvertFrom-Json).update_url
+    $links = (Invoke-WebRequest -Uri "https://github.com$($latestRelease)" -UseBasicParsing).Links.href
+    $fileUri = 'https://github.com' + ( $links | Select-String -Pattern '.*x64.zip' | Get-Unique).Tostring().Trim()
+    $pwsh7Root = Join-Path $PowerShell7Path 'Latest'
+    Invoke-WebRequest -uri $fileUri -UseBasicParsing  -OutFile (Join-Path $WorkingFolder 'pwsh.zip') -Verbose
+    $pwsh7Ver = $fileUri -replace ".*Powershell-(7.*(\d+\.)?(\*|\d+).*)\.zip",'$1'
+}
+
+if ( $InstallDotnetInteractive ) {
+    Write-Verbose 'Downloading latest .NET Core SDK...'
+    $links = (Invoke-WebRequest -uri 'https://dotnet.microsoft.com/download' -UseBasicParsing).Links.href
+    $latestVer = (($links | Select-String -Pattern '.*sdk.*windows-x64-installer') -replace '.*sdk-(([0-9]+\.){1}[0-9]+(\.[0-9]+)?)-.*', '$1' | Measure-Object -Maximum).Maximum
+    $latestUri = 'https://dotnet.microsoft.com' + ($links | Select-String -Pattern ".*sdk-$latestVer-windows-x64-installer" | Get-Unique).Tostring().Trim()
+    $fileUri = ((Invoke-WebRequest -uri $latestUri -UseBasicParsing).Links.href | Select-String -Pattern '.*\.exe' | Get-Unique).Tostring().Trim()
+    Invoke-WebRequest -uri $fileUri -UseBasicParsing  -OutFile (Join-Path $WorkingFolder 'dotnet.exe') -Verbose
+}
+
 $progressPreference = 'Continue'
+
+Write-Verbose 'Installing WinPython...'
 New-Item -Path $WinPythonPath -ItemType Directory -Force
 Start-Process -FilePath (Join-Path $WorkingFolder 'Winpython.exe') -ArgumentList ('-y -o"' + $WinPythonPath + '"') -wait
-$wpVer = $fileUri -replace ".*-((\d+\.)?(\d+\.)?(\d+\.)?(\*|\d+)).*\.exe",'$1'
-$wpRoot = Join-Path $WinPythonPath "WPy$osBits-$($wpVer -replace('\.',''))"
+$wpRoot = (Join-Path $WinPythonPath 'Latest')
+New-Item -ItemType SymbolicLink -Path $wpRoot -Target (Join-Path $WinPythonPath "WPy$osBits-$($wpVer -replace('\.',''))") -Force
+
 $kernelPath = Join-Path $wpRoot '\settings\kernels'
 if ( $CleanupDownloadFiles ) {
     Start-Sleep -Seconds 5
     Remove-Item (Join-Path $WorkingFolder 'Winpython.exe') -Force
 }
 
-Write-Output '##### Node.js Installation #####'
-$links = (Invoke-WebRequest -uri 'https://nodejs.org/ja/download/' -UseBasicParsing).Links.href
-$pattern = "win-x$osBits.*\.zip"
-$fileUri = ($links | Select-String -Pattern $pattern | Get-Unique).Tostring().Trim()
-$progressPreference = 'SilentlyContinue'
-Invoke-WebRequest -Uri $fileUri -OutFile (Join-Path $WorkingFolder '\node.zip')
-$progressPreference = 'Continue'
+Write-Verbose 'Installing Node.js...'
 New-Item -Path $NodePath -ItemType Directory -Force
 Expand-Archive -Path (Join-Path $WorkingFolder '\node.zip') -DestinationPath $NodePath -Force
 [Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') > $null
 $nodeFolder = [IO.Compression.ZipFile]::OpenRead("$(Join-Path $WorkingFolder '\node.zip')").Entries[0].FullName -replace('/','')
-. (join-path $NodePath "$nodeFolder\nodevars.bat")
-$env:Path += (';' + (join-path $nodePath "node-v$nodeVer-win-x64"))
-$nodeEnvPath = join-path $nodePath $nodeFolder
+$nodeEnvPath = join-path $nodePath 'Latest'
+New-Item -ItemType SymbolicLink -Path $nodeEnvPath -Target (join-path $nodePath $nodeFolder) -Force
+. (join-path $nodeEnvPath "nodevars.bat")
+$env:Path += (';' + $nodeEnvPath)
 @"
 set NODEPATH=$nodeEnvPath
 echo ";%PATH%;" | %FINDDIR%\find.exe /C /I ";%NODEPATH%\;" >nul
@@ -74,14 +128,7 @@ if ( $CleanupDownloadFiles -and $downloaded ) {
 }
 
 if ( $InstallPortableGit ) {
-    Write-Output '##### PortableGit Installation #####'
-    $latestRelease = (Invoke-WebRequest 'https://github.com/git-for-windows/git/releases/latest' -UseBasicParsing -Headers @{'Accept'='application/json'}| ConvertFrom-Json).update_url
-    $links = (Invoke-WebRequest -uri "https://github.com$($latestRelease)" -UseBasicParsing).Links.href
-    $fileUri = ($links | Select-String -Pattern ".*PortableGit.*$osBits.*\.exe" | Get-Unique).Tostring().Trim()
-    $progressPreference = 'SilentlyContinue'
-    Write-Output 'Downloading PortableGit...'
-    Invoke-WebRequest -uri "https://github.com$($fileUri)" -OutFile (Join-Path $WorkingFolder 'PortableGit.exe') -Verbose
-    $progressPreference = 'Continue'
+    Write-Verbose 'Installing PortableGit...'
     New-Item -Path $PortableGitPath -ItemType Directory -Force
     Start-Process -FilePath (Join-Path $WorkingFolder 'PortableGit.exe') -ArgumentList ('-y -o"' + $PortableGitPath + '"') -wait
     $gitEnvPath = Join-Path $PortableGitPath 'cmd'
@@ -100,21 +147,16 @@ if %ERRORLEVEL% NEQ 0 (
 "@ | Add-Content -Path "$wpRoot\scripts\env.bat"    
 }
 
-Write-Output '##### Jupyter Installation #####'
+Write-Verbose 'Installing Jupyter...'
 & "$wpRoot\scripts\env_for_icons.bat"
 . "$wpRoot\scripts\WinPython_PS_Prompt.ps1"
 pip install jupyter
-pip install jupyterhub
 pip install jupyterlab
 pip install powershell_kernel
 python -m powershell_kernel.install
-
-if ( $ReplaceToAndrewguKernel ) {
-    Rename-Item -Path (Join-Path $env:WINPYDIR 'Lib\site-packages\powershell_kernel\powershell_proxy.py') -NewName 'powershell_proxy.py.org' -Force
-    Invoke-WebRequest -UseBasicParsing `
-        -Uri 'https://raw.githubusercontent.com/andrewgu/jupyter-powershell/master/powershell_kernel/powershell_proxy.py' `
-        -OutFile (Join-Path $env:WINPYDIR 'Lib\site-packages\powershell_kernel\powershell_proxy.py')
-}
+$fileContent = Get-Content "$env:WINPYDIR\Lib\site-packages\powershell_kernel\powershell_proxy.py" -Raw
+$fileContent = $filecontent -replace '\^','\a'
+$filecontent | Set-Content "$env:WINPYDIR\Lib\site-packages\powershell_kernel\powershell_proxy.py" -Force
 if ( $InstallNBExtensions ) {
     pip install jupyter_nbextensions_configurator
     jupyter nbextensions_configurator enable
@@ -143,18 +185,9 @@ if ( $InstallNIIExtensions ) {
 }
 
 if ( $InstallPowerShell7 ) {
-    Write-Output '##### PowerShell 7 Installation #####'
-    $latestRelease = (Invoke-WebRequest -Uri 'https://github.com/PowerShell/PowerShell/releases/latest' -UseBasicParsing -Headers @{'Accept'='application/json'}| ConvertFrom-Json).update_url
-    $links = (Invoke-WebRequest -Uri "https://github.com$($latestRelease)" -UseBasicParsing).Links.href
-    $fileUri = 'https://github.com' + ( $links | Select-String -Pattern '.*x64.zip' | Get-Unique).Tostring().Trim()
-    $pwsh7Ver = $fileUri -replace ".*Powershell-(7.*(\d+\.)?(\*|\d+).*)\.zip",'$1'
-    $pwsh7Root = Join-Path $PowerShell7Path $pwsh7Ver
-    $progressPreference = 'silentlyContinue'
-    Write-Output 'Downloading latest PowerShell 7...'
-    Invoke-WebRequest -uri $fileUri -UseBasicParsing  -OutFile (Join-Path $WorkingFolder 'pwsh.zip') -Verbose
-    $progressPreference = 'Continue'
-    Write-Output 'Installing PowerShell 7...'
-    Expand-Archive -Path (Join-Path $WorkingFolder 'pwsh.zip') -DestinationPath $pwsh7Root -Force
+    Write-Verbose 'Installing PowerShell 7...'
+    Expand-Archive -Path (Join-Path $WorkingFolder 'pwsh.zip') -DestinationPath (Join-Path $PowerShell7Path $pwsh7Ver) -Force
+    New-Item -ItemType SymbolicLink -Path $pwsh7Root -Target (Join-Path $PowerShell7Path $pwsh7Ver) -Force
     Set-Content -Value "@`"$pwsh7Root\pwsh.exe`" %*" -Path "$env:WINPYDIR\pwsh.cmd"
     Copy-Item -Path "$kernelPath\powershell" -Destination "$kernelPath\powershell7" -Recurse -Force
     $fileContent = Get-Content "$kernelPath\powershell7\kernel.json" -Raw
@@ -165,20 +198,10 @@ if ( $InstallPowerShell7 ) {
         Start-Sleep -Seconds 5
         Remove-Item (Join-Path $WorkingFolder 'pwsh.zip') -Force
     }
-
 }
 
 if ( $InstallDotnetInteractive ) {
-    Write-Output '##### .Net Interactive Installation #####'
-    $links = (Invoke-WebRequest -uri 'https://dotnet.microsoft.com/download' -UseBasicParsing).Links.href
-    $latestVer = (($links | Select-String -Pattern '.*sdk.*windows-x64-installer') -replace '.*sdk-(([0-9]+\.){1}[0-9]+(\.[0-9]+)?)-.*', '$1' | Measure-Object -Maximum).Maximum
-    $latestUri = 'https://dotnet.microsoft.com' + ($links | Select-String -Pattern ".*sdk-$latestVer-windows-x64-installer" | Get-Unique).Tostring().Trim()
-    $fileUri = ((Invoke-WebRequest -uri $latestUri -UseBasicParsing).Links.href | Select-String -Pattern '.*\.exe' | Get-Unique).Tostring().Trim()
-    Write-Output 'Downloading latest .NET Core SDK...'
-    $progressPreference = 'SilentlyContinue'
-    Invoke-WebRequest -uri $fileUri -UseBasicParsing  -OutFile (Join-Path $WorkingFolder 'dotnet.exe') -Verbose
-    $progressPreference = 'Continue'
-    Write-Output 'Installing .NET Core SDK...'
+    Write-Verbose 'Installing .NET Core SDK...'
     Start-Process -FilePath (Join-Path $WorkingFolder 'dotnet.exe') -ArgumentList '/install /passive /norestart' -Wait
     Write-Output 'Installing .NET Interactive...'
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
