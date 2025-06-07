329c43f1ac309fd8a00453b6d8bab18946a70285
Kazutaka Sakoda <kazutaka.sakoda@gmail.com>
2025-06-07 23:18:37 +0900

    Update Anaconda.ps1

--- Anaconda/Anaconda.ps1
+++ Anaconda/Anaconda.ps1
@@ -4,14 +4,14 @@ Param(
     [String]$InstallationType = 'Computer',
     [Switch]$InstallPwsh7SDK,
     [Switch]$InstallDotnetInteractive,
-    [Switch]$InstallNBExtensions,
-    [Switch]$UsePipKernel,
     [Switch]$CleanupDownloadFiles,
     [String]$WorkingFolder = $PSScriptRoot
 )
 $ErrorActionPreference = 'Stop'
 [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'
 Push-Location $WorkingFolder
+chcp 65001
+$OutputEncoding = [System.Console]::OutputEncoding = [System.Console]::InputEncoding = [System.Text.Encoding]::GetEncoding('utf-8')
 $psBoundParameters.Keys | ForEach-Object {
     if ($($PSBoundParameters.$_.GetType().Name) -eq 'SwitchParameter') {
         $paramStrings += " -$_"
@@ -20,7 +20,7 @@ $psBoundParameters.Keys | ForEach-Object {
         $paramStrings += " -$_ $($PSBoundParameters.$_)"
     }
 }
-Write-Verbose "Commandline: `"$PSCommandPath`"$paramStrings"
+Write-Host "Commandline: `"$PSCommandPath`"$paramStrings"
 switch ($InstallationType) {
     { @('system', 'computer', 'allusers') -contains $_ } {
         if (-not([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
@@ -32,7 +32,7 @@ switch ($InstallationType) {
         $dataPath = $env:ProgramData
         $kernelPath = Join-Path $env:ProgramData '\Anaconda3\share\jupyter\kernels'
         $packagePath = Join-Path $env:ProgramData '\Anaconda3\Lib\site-packages'
-        $pyTypeOpt = '--sys-prefix'
+        #$pyTypeOpt = '--sys-prefix'
         [System.Environment]::SetEnvironmentVariable('PYTHONUTF8',1,[System.EnvironmentVariableTarget]::Machine)
     }
     { @('user', 'justme') -contains $_ } {
@@ -40,7 +40,7 @@ switch ($InstallationType) {
         $dataPath = $env:UserProfile
         $kernelPath = Join-Path $env:AppData '\jupyter\kernels'
         $packagePath = Join-Path $env:USERPROFILE '\Anaconda3\Lib\site-packages'
-        $pyTypeOpt = '--user'
+        #$pyTypeOpt = '--user'
         [System.Environment]::SetEnvironmentVariable('PYTHONUTF8',1,[System.EnvironmentVariableTarget]::User)
     }
     default {
@@ -54,24 +54,13 @@ if ($ProgressPreference -ne 'SilentlyContinue') {
     $ProgressPreference = 'SilentlyContinue'
 }
 
-Write-Verbose 'Downloading latest Anaconda...'
+Write-Host 'Downloading latest Anaconda...'
 $links = (Invoke-WebRequest -Uri 'https://www.anaconda.com/download/success' -UseBasicParsing).Links.href
-$fileUri = ($links | Select-String -Pattern '.*Windows-x86_64.exe' | Get-Unique).Tostring().Trim()
+$fileUri = ($links | Select-String -Pattern '.*Anaconda3.*Windows-x86_64.exe' | Get-Unique).Tostring().Trim()
 Invoke-WebRequest -Uri $fileUri -UseBasicParsing -OutFile (Join-Path $WorkingFolder 'anaconda.exe') -Verbose
 
-if ($InstallPwsh7ForPipKernel) {
-    Write-Verbose 'Downloading latest PowerShell 7...'
-    $releaseURI = 'https://github.com/PowerShell/PowerShell/releases'
-    $latestRelease = (Invoke-WebRequest -Uri "$releaseURI/latest" -UseBasicParsing -Headers @{'Accept'='application/json'}| ConvertFrom-Json).update_url
-    $versionString = $latestRelease -replace '.*tag/(.*)', '$1'
-    $links = (Invoke-WebRequest -Uri "$releaseURI/expanded_assets/$($versionString)" -UseBasicParsing).Links.href
-    $fileUri = 'https://github.com' + ($links | Select-String -Pattern '.*x64.msi' | Get-Unique).Tostring().Trim()
-    Write-Verbose "Download from $fileUri"
-    Invoke-WebRequest -Uri $fileUri -UseBasicParsing -OutFile (Join-Path $WorkingFolder 'pwsh.msi') -Verbose
-}
-
 if ($InstallDotnetInteractive) {
-    Write-Verbose 'Downloading latest .NET Core SDK...'
+    Write-Host 'Downloading latest .NET Core SDK...'
     $links = (Invoke-WebRequest -Uri 'https://dotnet.microsoft.com/download' -UseBasicParsing).Links.href
     $latestVer = (($links | Select-String -Pattern '.*sdk.*windows-x64-installer') -replace '.*sdk-(([0-9]+\.){1}[0-9]+(\.[0-9]+)?)-.*', '$1' | Measure-Object -Maximum).Maximum
     $latestUri = 'https://dotnet.microsoft.com' + ($links | Select-String -Pattern ".*sdk-$latestVer-windows-x64-installer" | Get-Unique).Tostring().Trim()
@@ -79,7 +68,7 @@ if ($InstallDotnetInteractive) {
     Invoke-WebRequest -Uri $fileUri -UseBasicParsing -OutFile (Join-Path $WorkingFolder 'dotnet.exe') -Verbose
 }
 elseif ($InstallPwsh7SDK) {
-    Write-Verbose 'Downloading latest .NET Runtime...'
+    Write-Host 'Downloading latest .NET Runtime...'
     $links = (Invoke-WebRequest -Uri 'https://dotnet.microsoft.com/en-us/download/dotnet/8.0/runtime' -UseBasicParsing).Links.href
     $latestVer = (($links | Select-String -Pattern '.*runtime.*windows-x64-installer') -replace '.*runtime-(([0-9]+\.){1}[0-9]+(\.[0-9]+)?)-.*', '$1' | Measure-Object -Maximum).Maximum
     $latestUri = 'https://dotnet.microsoft.com' + ($links | Select-String -Pattern ".*runtime-$latestVer-windows-x64-installer" | Get-Unique).Tostring().Trim()
@@ -87,25 +76,23 @@ elseif ($InstallPwsh7SDK) {
     Invoke-WebRequest -Uri $fileUri -UseBasicParsing -OutFile (Join-Path $WorkingFolder 'dotnet.exe') -Verbose
 }
 
-if (-not($UsePipKernel)) {
-    $releaseURI = 'https://github.com/sakaztk/Jupyter-PowerShellSDK/releases'
-    $latestRelease = (Invoke-WebRequest -Uri "$releaseURI/latest" -UseBasicParsing -Headers @{'Accept'='application/json'}| ConvertFrom-Json).update_url
-    $versionString = $latestRelease -replace '.*tag/(.*)', '$1'
-    $links = (Invoke-WebRequest -Uri "$releaseURI/expanded_assets/$($versionString)" -UseBasicParsing).Links.href
-    Write-Verbose 'Downloading latest DeepAQ pwsh5 Kernel...'
-    $fileUri = 'https://github.com' + ( $links | Select-String -Pattern '.*PowerShell5.zip' | Get-Unique).Tostring().Trim()
-    Invoke-WebRequest -uri $fileUri -UseBasicParsing -OutFile (Join-Path $WorkingFolder 'PowerShell5.zip') -Verbose
-    Write-Verbose 'Downloading latest DeepAQ pwshSDK Kernel...'
-    $fileUri = 'https://github.com' + ( $links | Select-String -Pattern 'Jupyter-PowerShellSDK-7.*\.zip' | Get-Unique).Tostring().Trim()
-    Invoke-WebRequest -uri $fileUri -UseBasicParsing -OutFile (Join-Path $WorkingFolder 'PowerShellSDK.zip') -Verbose
-}
+$releaseURI = 'https://github.com/sakaztk/Jupyter-PowerShellSDK/releases'
+$latestRelease = (Invoke-WebRequest -Uri "$releaseURI/latest" -UseBasicParsing -Headers @{'Accept'='application/json'}| ConvertFrom-Json).update_url
+$versionString = $latestRelease -replace '.*tag/(.*)', '$1'
+$links = (Invoke-WebRequest -Uri "$releaseURI/expanded_assets/$($versionString)" -UseBasicParsing).Links.href
+Write-Host 'Downloading latest DeepAQ pwsh5 Kernel...'
+$fileUri = 'https://github.com' + ( $links | Select-String -Pattern '.*PowerShell5.zip' | Get-Unique).Tostring().Trim()
+Invoke-WebRequest -uri $fileUri -UseBasicParsing -OutFile (Join-Path $WorkingFolder 'PowerShell5.zip') -Verbose
+Write-Host 'Downloading latest DeepAQ pwshSDK Kernel...'
+$fileUri = 'https://github.com' + ( $links | Select-String -Pattern 'Jupyter-PowerShellSDK-7.*\.zip' | Get-Unique).Tostring().Trim()
+Invoke-WebRequest -uri $fileUri -UseBasicParsing -OutFile (Join-Path $WorkingFolder 'PowerShellSDK.zip') -Verbose
 
 if ($null -ne $exProgressPreference) {
     Write-Verbose "Restore $ProgressPreference to $exProgressPreference"
     $ProgressPreference = $exProgressPreference
 }
 
-Write-Output 'Installing Anaconda. this may take survival minutes...'
+Write-Host 'Installing Anaconda. this may take survival minutes...'
 $process = Start-Process -FilePath 'anaconda.exe' -ArgumentList $condaOpt -PassThru
 for($i = 0; $i -le 100; $i = ($i + 1) % 100) {
     Write-Progress -Activity 'Installer' -PercentComplete $i -Status 'Installing...'
@@ -119,10 +106,10 @@ if ($CleanupDownloadFiles) {
     Start-Sleep -Seconds 5
     Remove-Item 'anaconda.exe' -Force
 }
-Write-Output '...Done'
+Write-Host '...Done'
 & "$dataPath\Anaconda3\shell\condabin\conda-hook.ps1"
 conda activate
-Write-Verbose '##### Jupyter Installation #####'
+Write-Host '##### Jupyter Installation #####'
 conda upgrade -y pip
 conda upgrade -y wheel
 conda install -y jupyter
@@ -130,57 +117,33 @@ conda install -y notebook
 conda install -y jupyterlab
 conda install -y -c conda-forge nodejs
 
-if ($InstallNBExtensions) {
-    conda install -y -c conda-forge jupyter_nbextensions_configurator
-    jupyter nbextensions_configurator enable
-    pip install https://github.com/ipython-contrib/jupyter_contrib_nbextensions/tarball/master
-    jupyter contrib nbextension install $pyTypeOpt    
-}
-if ($UsePipKernel) {
-    pip install powershell_kernel
-    python -m powershell_kernel.install $pyTypeOpt
-    if ($InstallPwsh7ForPipKernel) {
-        Write-Verbose 'Installing PowerShell 7...'
-        Start-Process -FilePath 'pwsh.msi' -ArgumentList '/passive' -Wait
-        Copy-Item -Path "$kernelPath\powershell" -Destination "$kernelPath\powershell7" -Recurse -Force
-        $fileContent = Get-Content "$kernelPath\powershell7\kernel.json" -Raw
-        $fileContent = $filecontent -replace '"display_name": "[^"]*"','"display_name": "PowerShell 7"'
-        $fileContent = $filecontent -replace '"powershell_command": "[^"]*"',"`"powershell_command`": `"pwsh.exe`""
-        $filecontent | Set-Content "$kernelPath\powershell7\kernel.json"
-        if ($CleanupDownloadFiles) {
-            Start-Sleep -Seconds 5
-            Remove-Item 'pwsh.msi' -Force
-        }
-    }
-}
-else {
-    Write-Verbose 'Installing DeepAQ pwsh5 Kernel...'
-    $installPath = Join-Path $packagePath 'powershell5_kernel'
-    Expand-Archive -Path (Join-Path $WorkingFolder 'PowerShell5.zip') -DestinationPath $installPath -Force
-    New-Item -ItemType Directory -Path (Join-Path $kernelPath '\powershell5\') -Force
-    Invoke-WebRequest -UseBasicParsing -Verbose -Uri 'https://raw.githubusercontent.com/PowerShell/PowerShell/master/assets/Powershell_64.png' -OutFile (Join-Path $kernelPath '\powershell5\logo-64x64.png')
-    Add-Type -AssemblyName System.Drawing
-    $image = [System.Drawing.Image]::FromFile((Join-Path $kernelPath '\powershell5\logo-64x64.png'))
-    $bitmap32 = New-Object System.Drawing.Bitmap(32, 32)
-    [System.Drawing.Graphics]::FromImage($bitmap32).DrawImage($image, 0, 0, 32, 32)
-    $bitmap32.Save((Join-Path $kernelPath '\powershell5\logo-32x32.png'), [System.Drawing.Imaging.ImageFormat]::Png)
+Write-Host 'Installing DeepAQ pwsh5 Kernel...'
+$installPath = Join-Path $packagePath 'powershell5_kernel'
+Expand-Archive -Path (Join-Path $WorkingFolder 'PowerShell5.zip') -DestinationPath $installPath -Force
+New-Item -ItemType Directory -Path (Join-Path $kernelPath '\powershell5\') -Force
+Invoke-WebRequest -UseBasicParsing -Verbose -Uri 'https://raw.githubusercontent.com/PowerShell/PowerShell/master/assets/Powershell_64.png' -OutFile (Join-Path $kernelPath '\powershell5\logo-64x64.png')
+Add-Type -AssemblyName System.Drawing
+$image = [System.Drawing.Image]::FromFile((Join-Path $kernelPath '\powershell5\logo-64x64.png'))
+$bitmap32 = New-Object System.Drawing.Bitmap(32, 32)
+[System.Drawing.Graphics]::FromImage($bitmap32).DrawImage($image, 0, 0, 32, 32)
+$bitmap32.Save((Join-Path $kernelPath '\powershell5\logo-32x32.png'), [System.Drawing.Imaging.ImageFormat]::Png)
 @"
 {
-  "argv": [
-    "$($installPath.replace('\','/'))/Jupyter_PowerShell5.exe",
-    "{connection_file}"
-  ],
-  "display_name": "PowerShell 5",
-  "language": "Powershell"
+    "argv": [
+        "$($installPath.replace('\','/'))/Jupyter_PowerShell5.exe",
+        "{connection_file}"
+    ],
+    "display_name": "PowerShell 5",
+    "language": "Powershell"
 }
 "@ | Set-Content -Path (Join-Path $kernelPath '\powershell5\kernel.json')
-    Move-Item -Path (Join-Path $installPath '*.png') -Destination (Join-Path $kernelPath '\powershell5\') -Force
-    if ($CleanupDownloadFiles) {
-        Remove-Item (Join-Path $WorkingFolder 'PowerShell5.zip') -Force
-    }
+Move-Item -Path (Join-Path $installPath '*.png') -Destination (Join-Path $kernelPath '\powershell5\') -Force
+if ($CleanupDownloadFiles) {
+    Remove-Item (Join-Path $WorkingFolder 'PowerShell5.zip') -Force
 }
+
 if ($InstallPwsh7SDK) {
-    Write-Verbose 'Installing DeepAQ pwshSDK Kernel...'
+    Write-Host 'Installing DeepAQ pwshSDK Kernel...'
     $installPath = Join-Path $packagePath 'powershellSDK_kernel'
     Expand-Archive -Path (Join-Path $WorkingFolder 'PowerShellSDK.zip') -DestinationPath $installPath -Force
     New-Item -ItemType Directory -Path (Join-Path $kernelPath '\powershellSDK\') -Force
@@ -192,12 +155,12 @@ if ($InstallPwsh7SDK) {
     $bitmap32.Save((Join-Path $kernelPath '\powershellSDK\logo-32x32.png'), [System.Drawing.Imaging.ImageFormat]::Png)
 @"
 {
-  "argv": [
-    "$($installPath.replace('\','/'))/Jupyter_PowerShellSDK.exe",
-    "{connection_file}"
-  ],
-  "display_name": "PowerShell 7 (SDK)",
-  "language": "Powershell"
+    "argv": [
+        "$($installPath.replace('\','/'))/Jupyter_PowerShellSDK.exe",
+        "{connection_file}"
+    ],
+    "display_name": "PowerShell 7 (SDK)",
+    "language": "Powershell"
 }
 "@ | Set-Content -Path (Join-Path $kernelPath '\powershellSDK\kernel.json')
     Move-Item -Path (Join-Path $installPath '*.png') -Destination (Join-Path $kernelPath '\powershellSDK\') -Force
@@ -207,9 +170,9 @@ if ($InstallPwsh7SDK) {
 }
 
 if ($InstallDotnetInteractive) {
-    Write-Verbose 'Installing .NET SDK...'
+    Write-Host 'Installing .NET SDK...'
     Start-Process -FilePath 'dotnet.exe' -ArgumentList '/install /passive /norestart' -Wait
-    Write-Verbose 'Installing .NET Interactive...'
+    Write-Host 'Installing .NET Interactive...'
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
     dotnet tool install -g --add-source "https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-tools/nuget/v3/index.json" Microsoft.dotnet-interactive
     dotnet interactive jupyter install --path "$kernelPath"
@@ -219,7 +182,7 @@ if ($InstallDotnetInteractive) {
     }
 }
 elseif ($InstallPwsh7SDK) {
-    Write-Verbose 'Installing .NET Runtime...'
+    Write-Host 'Installing .NET Runtime...'
     Start-Process -FilePath (Join-Path $WorkingFolder 'dotnet.exe') -ArgumentList '/install /passive /norestart' -Wait
     if ($CleanupDownloadFiles) {
         Start-Sleep -Seconds 5
--- Anaconda/README-JP.md
+++ Anaconda/README-JP.md
@@ -3,13 +3,9 @@
 
 ## インストールするソフトウェア
 - [Anaconda](https://www.anaconda.com/)
+- [Node.js](https://nodejs.org/) (JupyterLab拡張機能の要件)
 - [Jupyter-PowerShell5](https://github.com/DeepAQ/Jupyter-PowerShell5)
 - [Jupyter-PowerShellSDK](https://github.com/sakaztk/Jupyter-PowerShellSDK) (オプション)
-- [Jupyter Powershell Kernel](https://github.com/vors/jupyter-powershell) (オプション)
-- [Node.js](https://nodejs.org/) (Use in extentions for JupyterLab)
-- [PowerShell 7](https://github.com/PowerShell/PowerShell) (オプション)
-- [Jupyter Nbextensions](https://github.com/ipython-contrib/jupyter_contrib_nbextensions) (オプション)
-- [Jupyter Nbextensions Configurator](https://github.com/Jupyter-contrib/jupyter_nbextensions_configurator) (オプション)
 - [.Net Interactive](https://github.com/dotnet/interactive) (オプション)
 
 ## インストール方法
@@ -22,22 +18,9 @@
 デフォルト: Computer  
 例: .\Anaconda.ps1 -InstallationType User
 
-- UsePipKernel  
-このスイッチオプションを指定すると、[Jupyter-PowerShell5](https://github.com/DeepAQ/Jupyter-PowerShell5)の代わりに[Jupyter Powershell Kernel](https://github.com/vors/jupyter-powershell)を使用します。  
-例: .\Anaconda.ps1 -UsePipKernel
-
-- InstallPwsh7ForPipKernel  
-このスイッチオプションを指定すると、最新版の[PowerShell 7](https://github.com/PowerShell/PowerShell/releases/latest)をインストールし、[Jupyter Powershell Kernel](https://github.com/vors/jupyter-powershell)で使用可能にします。  
-例: .\Anaconda.ps1 -UsePipKernel -InstallPwsh7ForPipKernel
-
 - InstallPwsh7SDK  
 このスイッチオプションを指定すると、 [Jupyter-PowerShellSDK](https://github.com/sakaztk/Jupyter-PowerShellSDK)をインストールします。  
 例: .\Anaconda.ps1 -InstallPwsh7SDK
-
-- InstallNBExtensions  
-このスイッチオプションを指定すると、 [Jupyter Nbextensions](https://github.com/ipython-contrib/jupyter_contrib_nbextensions) と [Jupyter Nbextensions Configurator](https://github.com/Jupyter-contrib/jupyter_nbextensions_configurator)をインストールします。  
-[Note] インストールすることでJupyterLabのエクスポート機能が使えなくなる可能性があります。([*](https://github.com/jupyterlab/jupyterlab-desktop/issues/465))  
-例: .\Anaconda.ps1 -InstallNBExtensions
 
 - InstallDotnetInteractive  
 このスイッチオプションを指定すると、[.Net Interactive](https://github.com/dotnet/interactive)をインストールします。  
